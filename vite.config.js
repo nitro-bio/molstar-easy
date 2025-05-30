f2c6e034a9bd39d16aeb00906a0ebe69d4664025
Nishant Jha <github@nishantjha.org>
2025-05-30 09:48:52 -0700

    molstar is peer dep

--- vite.config.js
+++ vite.config.js
@@ -30,12 +30,34 @@ export default defineConfig({
       fileName: (format) => `molstar-easy.${format}.js`,
     },
     rollupOptions: {
-      external: ["react", "react-dom", "styled-components"],
+      external: ["react", "react-dom", "styled-components", /^molstar/],
       output: {
         globals: {
           react: "React",
           "react-dom": "ReactDOM",
           "styled-components": "styled",
+          molstar: "molstar",
+          "molstar/lib/mol-model/structure": "molstar.molModel.structure",
+          "molstar/lib/mol-theme/color/entity-id":
+            "molstar.molTheme.color.entityId",
+          "molstar/lib/mol-util/color": "molstar.molUtil.color",
+          "molstar/lib/mol-util/legend": "molstar.molUtil.legend",
+          "molstar/lib/mol-util/param-definition":
+            "molstar.molUtil.paramDefinition",
+          "molstar/build/viewer/molstar.css": "molstar.viewer.css",
+          "molstar/lib/mol-plugin-state/helpers/structure-overpaint":
+            "molstar.molPluginState.helpers.structureOverpaint",
+          "molstar/lib/mol-plugin-state/transforms/data":
+            "molstar.molPluginState.transforms.data",
+          "molstar/lib/mol-plugin-state/transforms/model":
+            "molstar.molPluginState.transforms.model",
+          "molstar/lib/mol-plugin-state/transforms/representation":
+            "molstar.molPluginState.transforms.representation",
+          "molstar/lib/mol-plugin/context": "molstar.molPlugin.context",
+          "molstar/lib/mol-plugin/spec": "molstar.molPlugin.spec",
+          "molstar/lib/mol-script/language/builder":
+            "molstar.molScript.language.builder",
+          "molstar/lib/mol-script/script": "molstar.molScript.script",
         },
         interop: "compat",
       },
